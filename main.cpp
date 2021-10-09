e65345cd0e0934ea3bd5d9bd8815777f16b6e2a7
Basar <74671944+Basarus@users.noreply.github.com>
2021-10-09 22:10:28 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+ int t = 0;   
+ cin >> t;
+ for (int i = 0; i < t; i++){
+     int a = 0;
+     int b = 0;
+     cin >> a >> b;
+     cout << a + b << endl;
+     
+ };
+ 
+    return 0;
+}
